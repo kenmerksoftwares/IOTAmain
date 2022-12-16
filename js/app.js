cc0c4b3fd8b1a95c1e5092976ca1373da9ed22cd
kenmerksoftwares <90622631+kenmerksoftwares@users.noreply.github.com>
2022-12-16 11:51:21 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,72 @@
+
+
+$(document).ready(function () {
+    //Owl
+    $('.hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        navText: ['PREV', 'NEXT'],
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        responsive: {
+            0: {
+                nav: false,
+            },
+            768: {
+                nav: true,
+            }
+        }
+    })
+
+    $('#projects-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 2,
+        dots: true,
+        smartSpeed: 600,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2,
+                margin: 8,
+            }
+        }
+    })
+
+    $('.reviews-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        smartSpeed: 900,
+        items: 1,
+        margin: 24,
+        autoplay: true,
+        autoplayTimeout: 4000,
+    })
+});
+$('#play-video').on('click', function(e){
+  e.preventDefault();
+  $('#video-overlay').addClass('open');
+  $("#video-overlay").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
+});
+
+$('.video-overlay, .video-overlay-close').on('click', function(e){
+  e.preventDefault();
+  close_video();
+});
+
+$(document).keyup(function(e){
+  if(e.keyCode === 27) { close_video(); }
+});
+
+function close_video() {
+  $('.video-overlay.open').removeClass('open').find('iframe').remove();
+};
